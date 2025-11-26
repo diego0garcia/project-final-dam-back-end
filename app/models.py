13f670ef4e9793b07836cf15dc9dd0199faf5da3
diego0garcia <dgjimenez986@gmail.com>
2025-11-26 16:13:33 +0100

    Addesd users API.

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+    password: str
+
+
+class UserIn(UserBase):
+    name: str
+    email: str
+    tlf: int
+    password: str
+    
+    
+class UserOut(BaseModel):
+    id: int
+    username: str
+    name: str
+    email: str
+    tlf: int
+    
+    
+class UserDb(UserIn):
+    id: int
+
+
+#Es como un alias de UserBase pq esta vacio
+class UserLoginIn(UserBase):
+    pass
+ 
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,31 +1,15 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Header, Depends
 from pydantic import BaseModel
+from app.auth.auth import Token
+from fastapi.security import OAuth2PasswordRequestForm
+from app.models import UserBase,UserDb,UserIn,UserLoginIn,UserOut
+from app.database import UserDb
+from app.auth.auth import create_access_token, verify_password
 
-router123 = APIRouter(
+router = APIRouter(
     prefix="/users",
     tags=["Users"] #Esto es para la documentacion
-)
-
-
-class UserBase(BaseModel):
-    username: str
-    password: str
-
-class UserIn(UserBase):
-    name: str
-    email: str
-    tlf: int
-    password: str
-    
-class UserDb(UserIn):
-    id: int
-
-#Es como un alias de UserBase pq esta vacio
-class UserLoginIn(UserBase):
-    pass
-    
-class TokenOut(BaseModel):
-    token: str
+)   
 
 users: list[UserDb] = []
 
@@ -33,7 +17,7 @@ users: list[UserDb] = []
 @router.post("/signup/", status_code=status.HTTP_201_CREATED)
 async def created_user(userIn: UserIn):
     usersFound = [u for u in users if u.username == userIn.username]
-    if len(userFound) > 0:
+    if len(usersFound) > 0:
         raise HTTPException(
             status_code = status.HTTP_409_CONFLICT,
             detail = "Username already exists"
@@ -48,26 +32,78 @@ async def created_user(userIn: UserIn):
             password = UserIn.password
         )
     )
+    
 
-@router.post("/login/",response_model=TokenOut, status_code=status.HTTP_200_OK)
-async def login_user(userLoginIn: UserLoginIn):
-    usersFound = [u for u in users if u.username == userLoginIn.username]
+@router.post("/login/", status_code=status.HTTP_200_OK)
+async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    username: str | None = form_data.get("username")
+    password: str | None = form_data.get("password")
+    
+    if username is None or password is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username and/or password incorrect"
+        )    
+    
+    usersFound = [u for u in users if u.username == username]
+    
     if not usersFound:
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Username and/or password incorrect"
         )
-        
+    
     user: UserDb = usersFound[0]
-    if user.password != userLoginIn.password:
-        raise HTTPException(
+    if  verify_password(password, user.password):
+            raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Username and/or password incorrect"
         )
     
-    return TokenOut(token=f"mytoken:{user.username}-{user.password}")
+    token = create_access_token(
+        UserBase(
+            username=user.username,
+            password=user.password
+        )
+    )
+    return token
     
 #Cuando accedas a /users/signup/ se ejecuta el seguiente metodo (created_user)
 @router.get("/{id}/", status_code=status.HTTP_201_CREATED)
 async def get_user_by_id(id: int):
     pass
+
+
+@router.get("/",response_model=list[UserOut],status_code=status.HTTP_200_OK)
+async def get_all_users(authorization: str | None = Header()):
+    print(authorization)
+    
+    parts = authorization.split(":")
+    if len(parts) != 2:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden"
+        )
+    
+    if parts[0] != "mytoken":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden"
+        )
+        
+    payload_parts = parts[1].split("-")
+    #Convierto un list[UserDb] en list[UserOut]
+    return[
+        
+    ]
+    
+    username = payload_parts[0]
+    if username not in [u.username for u in users]:
+      raise HTTPException(
+          status_code=status.HTTP_403_FORBIDDEN,
+          detail="Forbidden"
+      )  
+    
+    return [
+        UserOut(id=UserDb.id, name=userDB.name, username=UserDb.username, email=UserDb.email, tlf=UserDb.tlf)
+        for userDB in users]
