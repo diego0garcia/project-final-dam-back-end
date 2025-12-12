72df88696b76e0a0518f2e5784b27053859b8ccf
diego0garcia <dgjimenez986@gmail.com>
2025-12-12 16:37:10 +0100

    Terminados endpoints USERS.

--- app/database.py
+++ app/database.py
@@ -2,9 +2,6 @@ from app.models import UserDb
 import mariadb
 from app.models import UserDb
 
-users: list[UserDb] = [
-    UserDb(id=1, name="alice", username="alice", email="juan@gmail.com", tlf=687678899, password="$2b$12$CUgVgUwbXO9EuBm2FbTLie2aY/blRe6zuT0bEP40gL8NTSz3YYv.2")
-]
 
 db_config = {
     "host": "myapidb",
@@ -14,6 +11,7 @@ db_config = {
     "database": "myapi"
 }
 
+
 def insert_user(user: UserDb):
     with mariadb.connect(**db_config) as conn:
         with conn.cursor() as cursor:
@@ -22,27 +20,112 @@ def insert_user(user: UserDb):
             cursor.execute(sql, values)
             
     
-
-def get_user_by_username(username: str) -> UserDb | None:
+def get_all():
     with mariadb.connect(**db_config) as conn:
-        with conn.cusor() as cursor:
-            cursor.execute(insert_user,(3, "jose","jose","jose@gmail.com",356764567,"jose"))
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM users"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
             
-users: list[UserDb] = [
-    UserDb(
-        id=1,
-        name="Alice",
-        username="alice",
-        email="alice@gmail.com",
-        tlf=7658364593,
-        password="$2b$12$9DAIvp9W0ls6hY3mE.x.5elr0VsUgOpkFpQ3rp/4XOTPAckgaWztu"
-    ),
-    UserDb(
-        id=2,
-        name="Bob",
-        username="bob",
-        email="alice@gmail.com",
-        tlf=7658364593,
-        password="$2b$12$qs8F6B3JpINiXDAjhN6cYe9zTuHAE2xoeQ8NNvFtpCzIjg.iFzq3C"
-    )
-]
+            users = []
+            for row in rows:    
+                user = UserDb(
+                    id = row[0],
+                    username = row[1],
+                    name=row[2],
+                    email = row[3],
+                    tlf = row[4],
+                    password = row[5]
+                )
+                
+                users.append(user)
+                
+            return users
+        
+        
+def get_user_by_id(id: int):
+    with mariadb.connect(**db_config) as conn:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE id = ?"
+            values = (id,)
+            cursor.execute(sql, values)
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            
+            user = UserDb(
+                id = row[0],
+                username = row[1],
+                name=row[2],
+                email = row[3],
+                tlf = row[4],
+                password = row[5]
+            )
+            return user
+        
+        
+def delete_user_by_id(id: int):
+    with mariadb.connect(**db_config) as conn:
+        with conn.cursor() as cursor:
+            sql = "DELETE FROM users WHERE id = ?"
+            values = (id,)
+            cursor.execute(sql, values)
+            conn.commit()
+            return cursor.rowcount
+           
+
+def get_user_by_username(username: str):
+    with mariadb.connect(**db_config) as conn:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE username = ?"
+            values = (username,)
+            cursor.execute(sql, values)
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            
+            user = UserDb(
+                id = row[0],
+                username = row[1],
+                name=row[2],
+                email = row[3],
+                tlf = row[4],
+                password = row[5]
+            )
+            return user
+        
+
+def modify_user(id:int, name:str = None, username:str = None, email:str = None, tlf:str = None, password:str = None):
+    with mariadb.connect(**db_config) as conn:
+        with conn.cursor() as cursor:
+            
+            sql = "SELECT * FROM users WHERE id = ?"
+            values = (id,)
+            cursor.execute(sql, values)
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            
+            new_name = name if name is not None else row[1]
+            new_username = username if username is not None else row[2]
+            new_email = email if email is not None else row[3]
+            new_tlf = tlf if tlf is not None else row[4]
+            new_password = password if password is not None else row[5]
+            
+            sql = "UPDATE users SET username = ?, name = ?, email = ?, tlf = ?, password = ? WHERE id = ?"
+            values = (new_username, new_name, new_email, new_tlf, new_password, id)
+            cursor.execute(sql, values)
+            
+            user = UserDb(
+                id = id,
+                username = new_username,
+                name=new_name,
+                email = new_email,
+                tlf = new_tlf,
+                password = new_password
+            )
+            return user
+            
+                  
+#users: list[UserDb] = [UserDb(id=1,name="Alice",username="alice",email="alice@gmail.com",tlf=7658364593,password="$2b$12$9DAIvp9W0ls6hY3mE.x.5elr0VsUgOpkFpQ3rp/4XOTPAckgaWztu"),UserDb(id=2,name="Bob",username="bob",email="alice@gmail.com",tlf=7658364593,password="$2b$12$qs8F6B3JpINiXDAjhN6cYe9zTuHAE2xoeQ8NNvFtpCzIjg.iFzq3C")]
+#users: list[UserDb] = [UserDb(id=1, name="alice", username="alice", email="juan@gmail.com", tlf=687678899, password="$2b$12$CUgVgUwbXO9EuBm2FbTLie2aY/blRe6zuT0bEP40gL8NTSz3YYv.2")]
--- app/routers/users.py
+++ app/routers/users.py
@@ -5,7 +5,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from app.models import UserBase,UserDb,UserIn,UserLoginIn,UserOut
 from app.database import UserDb
 from app.auth.auth import create_access_token, verify_password, oauth2_scheme, TokenData
-from app.database import insert_user
+from app.database import insert_user, get_user_by_id, delete_user_by_id, get_all, get_user_by_username, modify_user
 
 router = APIRouter(
     prefix="/users",
@@ -18,7 +18,7 @@ users: list[UserDb] = []
 @router.post("/signup/", status_code=status.HTTP_201_CREATED)
 async def created_user(userIn: UserIn):
     usersFound = [u for u in users if u.username == userIn.username]
-    if UserDb:
+    if usersFound:
         raise HTTPException(
             status_code = status.HTTP_409_CONFLICT,
             detail = "Username already exists"
@@ -27,10 +27,11 @@ async def created_user(userIn: UserIn):
     insert_user(
         UserDb(
             id = len(users) + 1,
+            username = userIn.username,
             name=userIn.name,
             email = userIn.email,
             tlf = userIn.tlf,
-            password = UserIn.password
+            password = userIn.password
         )
     )
     
@@ -72,20 +73,65 @@ async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
 #Cuando accedas a /users/signup/ se ejecuta el seguiente metodo (created_user)
 @router.get("/{id}/", status_code=status.HTTP_201_CREATED)
 async def get_user_by_id(id: int):
-    pass
+    user = get_user_by_id(id)
+    
+    if user is None:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "User not already exists"
+        )
+    
+    return user
+
+@router.get("/username/{username}/", status_code=status.HTTP_201_CREATED)
+async def get_user_by_name(username: str):
+    user = get_user_by_username(username)
+    
+    if user is None:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "User not already exists"
+        )
+    
+    return user
 
 
 @router.get("/",response_model=list[UserOut],status_code=status.HTTP_200_OK)
-async def get_all_users(token: str = Depends(oauth2_scheme)):#authorization: str | None = Header()):
-    data: TokenData = decode_token(token)
+#async def get_all_users(token: str = Depends(oauth2_scheme)):#authorization: str | None = Header()):
+async def get_all_users():
+    users = []
+    users = get_all()
      
-    if data.username not in [u.username for u in users]:
-      raise HTTPException(
+    if not users:
+        raise HTTPException(
           status_code=status.HTTP_403_FORBIDDEN,
           detail="Forbidden"
-      )  
+        )  
     
-    return [
-        UserOut(id=UserDb.id, name=userDB.name, username=UserDb.username, email=UserDb.email, tlf=UserDb.tlf)
-        for userDB in users
-    ]
+    return users
+    
+#Modify
+@router.put("/username/modify/{id}/", status_code=status.HTTP_201_CREATED)
+async def get_user_by_name(id:int, name:str = None, username:str = None, email:str = None, tlf:str = None, password:str = None):
+    user = modify_user(id, name, username, email, tlf, password)
+    
+    if user is None:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "User not already exists"
+        )
+    
+    return user
+
+#Delete
+@router.delete("/{id}")
+async def delete_user_by_id(id: int):
+    result = delete_user_by_id(id)
+    
+    if result == 0:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "User not already exists"
+        )
+    
+    return True
